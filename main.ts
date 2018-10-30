6ba3b6bda45980dcc3b973cab41926eef524eb69
CaptainCredible <cpt.credible@gmail.com>
2018-10-30 10:45:38 +0100

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,29 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+
+
+/**
+  * Enumeration of motors.
+  */
+enum BBMotor {
+    //% block="left"
+    Left,
+    //% block="right"
+    Right,
+    //% block="all"
+    All
+}
+/**
+ * Custom blocks
+ */
+//% weight=10 color=#0fbc11 icon="\uf1b9"
+namespace transTest {
+/**
+  * Sound a buzz.
+  *
+  * @param flag Flag to set (0) for off and (1) for on.
+  */
+//% blockId="transTest_buzz" block="buzz sound %flag"
+//% weight=95
+export function buzz(flag: number): void {
+    pins.digitalWritePin(DigitalPin.P14, flag === 0 ? 0 : 1);
+}
+}
